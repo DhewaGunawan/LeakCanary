0b643935bca71f66241505f674866b03a4522306
Benedictus Betavian <betavianb@gmail.com>
2022-01-25 23:38:04 +0700

    Add Firebase ( Analytics, Crashlytics, Monitoring )

--- app/src/main/java/app/beta/pokemon/MainActivity.kt
+++ app/src/main/java/app/beta/pokemon/MainActivity.kt
@@ -3,6 +3,7 @@ package app.beta.pokemon
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import app.beta.pokemon.databinding.MainActivityBinding
+import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,10 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: MainActivityBinding
 
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = MainActivityBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         instance = this
+
+        analytics = FirebaseAnalytics.getInstance(baseContext)
     }
 }
